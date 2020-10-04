8397f45af48eb1a805d3d45f4dc88d0c913a61c8
eleobert <eleobertalmeidaporto@gmail.com>
2020-10-05 00:38:28 +0300

    add newton interpolator

--- interpolators.hpp
+++ interpolators.hpp
@@ -2,8 +2,8 @@
 #include <vector>
 #include <cassert>
 
+#include <functional>
 #include <numeric>
-
 
 
 template <typename Container>
@@ -23,6 +23,7 @@ auto linspace(typename Container::value_type a, typename Container::value_type b
     return res;
 }
 
+
 template<typename RandomAcessContainer>
 class lagrange
 {
@@ -31,7 +32,7 @@ class lagrange
     const RandomAcessContainer y;
 
     // function for polinomial i
-    double p(double xi, double i);
+    double p(double xi, size_t i);
 
 public:
     lagrange(const RandomAcessContainer& x, const RandomAcessContainer& y): x(x), y(y)
@@ -56,13 +57,14 @@ public:
     double operator()(double x);
 };
 
+
 template<typename RandomAcessContainer>
-double lagrange<RandomAcessContainer>::p(double xi, double i)
+double lagrange<RandomAcessContainer>::p(double xi, size_t i)
 {
     auto num = 1.0;
-    for(int j = 0; j < x.size(); j++)
+    for(size_t j = 0; j < x.size(); j++)
     {
-        if( i != j)
+        if(i != j)
         {
             num *= xi - x[j];
         }
@@ -77,6 +79,55 @@ double lagrange<RandomAcessContainer>::operator()(double xi)
     for(int i = 0; i < x.size(); i++)
     {
         res += p(xi, i);
+    }
+    return res;
+}
+
+
+template<typename RandomAcessContainer>
+class newton
+{
+    std::vector<std::vector<double>> divided_differences;
+    double x0, y0, step;
+
+public:
+
+    newton(double x0, double step, const RandomAcessContainer& ys);
+
+    double operator()(double x);
+};
+
+
+template<typename RandomAcessContainer>
+newton<RandomAcessContainer>::newton(double x0, double step, const RandomAcessContainer& ys): 
+                                            step(step), x0(x0), y0(ys[0]), divided_differences(ys.size())
+{
+    divided_differences[0].resize(ys.size());
+    std::copy(ys.begin(), ys.end(), divided_differences[0].begin());
+    
+    for(size_t i = 1; i < ys.size(); i++)
+    {
+        auto& pdd = divided_differences[i - 1]; // previous divided differences
+        auto& cdd = divided_differences[i];     // current  divided differences
+        
+        cdd.resize(pdd.size() - 1);
+
+        std::transform(std::next(pdd.begin()), pdd.end(), pdd.begin(), cdd.begin(), std::minus{});
+    }
+}
+
+
+template<typename RandomAcessContainer>
+double newton<RandomAcessContainer>::operator()(double x)
+{
+    auto res  = y0;
+    auto q    = (x - x0) / step;
+    auto poly = q;
+
+    for(int i = 1; i < divided_differences.size(); i++)
+    {
+        res  += poly * divided_differences[i][0];
+        poly *= (q - i) / (i + 1);
     }
     return res;
 }
